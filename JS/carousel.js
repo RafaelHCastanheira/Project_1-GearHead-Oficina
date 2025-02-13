87c02abb71c5151e8b7d481be26114657b439fbf
RafaelHCastanheira <castanheirarafael1@outlook.com>
2025-02-13 00:37:09 -0300

    Modificações carrossel, melhorias visuais. V.1.0.5

--- JS/carousel.js
+++ JS/carousel.js
@@ -2,6 +2,15 @@ document.addEventListener("DOMContentLoaded", () => {
     let slides = document.querySelectorAll('.slide');
     let pontos = document.querySelectorAll('.ponto');
     let currentSlide = 0;
+    let autoPlay = true;
+    let slideInterval;
+
+    function initCarousel() {
+        showSlide(currentSlide);
+        if (autoPlay) {
+            startInterval();
+        }
+    }
 
     function showSlide(n) {
         // Remove a classe "active" de todos os slides e pontos
@@ -14,19 +23,40 @@ document.addEventListener("DOMContentLoaded", () => {
         pontos[currentSlide].classList.add('active');
     }
 
+    function startInterval() {
+        slideInterval = setInterval(() => {
+            showSlide(currentSlide + 1);
+        }, 5000);
+    }
+
+    function resetInterval() {
+        clearInterval(slideInterval);
+        startInterval();
+    }
+
+
     // Event listeners para os botões
     document.querySelector('.next').addEventListener('click', () => {
         showSlide(currentSlide + 1);
+        resetInterval();
     });
 
     document.querySelector('.prev').addEventListener('click', () => {
         showSlide(currentSlide - 1);
+        resetInterval();
     });
 
     // Event listeners para os pontos
     pontos.forEach((ponto, index) => {
         ponto.addEventListener('click', () => {
             showSlide(index);
+            resetInterval();
         });
+        ponto.addEventListener('keypress', (e) => {
+            if (e.key === 'Enter') showSlide(index);
     });
+});
+
+// Iniciar
+initCarousel();
 });
